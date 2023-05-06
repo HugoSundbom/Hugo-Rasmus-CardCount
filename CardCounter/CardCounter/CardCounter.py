9022d3926cea07b2368986cddb14d1e118865581
unknown <hugo.sundbom@elev.ga.ntig.se>
2023-05-06 20:55:01 +0200

    1

--- CardCounter/CardCounter/CardCounter.py
+++ CardCounter/CardCounter/CardCounter.py
@@ -1,22 +1,45 @@
 
 print("KORTRÄKNARE")
+
+#Skapar kortleken
 färger = ["Hjärter", "Ruter", "Klöver", "Spader"]
 valörer = ["Äss", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Knäckt", "Dam", "Kung"]
 kortlek = []
-
 for valör in valörer:
     for färg in färger:
         kort = f"{färg} {valör}"
         kortlek.append(kort)
-spelat_kort = input('vilket kort har spelats?\n')
-print(spelat_kort)
-listLength = len(kortlek)
-while 1 < 2:
-    if spelat_kort in kortlek:
-        kortlek.remove(spelat_kort)
-        exit
+
+dealerSumma = 0
+antalSpelare = input('Hur många spelare spelar?\n> ')
+
+#Medan du spelar
+while 10 > 0:
+    spelatKort = input('vilket kort har spelats?\n> ')
+
+    while 1 < 2:
+        if spelatKort in kortlek:
+            kortlek.remove(spelatKort)
+            break
+        else:
+            spelatKort = input('Vänligen skriv ett fungernade kort\n>')
+    
+    #Dealeromgång
+    if dealerSumma < 16:
+        spelatKort = input('vilket kort drog dealern?\n> ')
+        #LÄGG TILL NY BERÄKNING AV DEALER SUMMA; BUGG
     else:
-        spelat_kort = input("Vänligen skriv ett fungernade kort ")
+        print('Dealern har stannat')
 
 
 print(kortlek)
+
+#Kolla vilket kort det givna är
+def kortKollare(kort):
+    splittad = [x.split() for x in kort.split(' ')]
+    if splittad[1] == 'Äss':
+        return 1
+    elif splittad[1] == 'Knäckt' or splittad[1] == 'Dam' or splittad[1] == 'Kung':
+        return 10
+    else:
+        return splittad[1]
